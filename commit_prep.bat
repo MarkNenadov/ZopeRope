1c5171e6ec659af617068bfdd20d40c6c0be9548
Mark Nenadov <marknenadov@gmail.com>
2011-04-14 10:42:38 -0400

    Added pydocs and a commit preparation batch file

---
+++ commit_prep.bat
@@ -0,0 +1,9 @@
+del *.py~
+
+c:\python27\python c:\python27\lib\pydoc.py -w zoperope_support_lib
+c:\python27\python c:\python27\lib\pydoc.py -w zope2_fetch_objs
+c:\python27\python c:\python27\lib\pydoc.py -w zope2_gather_info
+
+move zoperope_support_lib.html pydocs
+move zope2_fetch_objs.html pydocs
+move zope2_gather_Info.html pydocs
