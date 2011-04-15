c263e4b80d70d1bb0be1daa9a6c8ac30f3fdbeec
Mark Nenadov <marknenadov@gmail.com>
2011-04-15 06:14:37 -0400

    Moved api docs from pydocs to api_doc and updated commit_prep.bat

--- commit_prep.bat
+++ commit_prep.bat
@@ -4,6 +4,6 @@ c:\python27\python c:\python27\lib\pydoc.py -w zoperope_support_lib
 c:\python27\python c:\python27\lib\pydoc.py -w zope2_fetch_objs
 c:\python27\python c:\python27\lib\pydoc.py -w zope2_gather_info
 
-move zoperope_support_lib.html pydocs
-move zope2_fetch_objs.html pydocs
-move zope2_gather_Info.html pydocs
+move zoperope_support_lib.html api_doc
+move zope2_fetch_objs.html api_doc
+move zope2_gather_Info.html api_doc
